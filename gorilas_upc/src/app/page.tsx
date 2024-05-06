03d8c46de2c3c2066d0de302ce0a5382f4c27898
Fez248 <ferran.beneitez@estudiantat.upc.edu>
2024-05-07 00:08:05 +0200

    Start

---
+++ gorilas_upc/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Image } from "@nextui-org/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="w-200 h-200 bg-skye-200">
+      </div>
+      <p>Hello</p>
+    </div>
+  );
+}
