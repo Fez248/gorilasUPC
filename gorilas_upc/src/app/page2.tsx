5efcffac53246823f5a5b7aff64928c5de028458
Fez248 <ferran.beneitez@estudiantat.upc.edu>
2024-05-17 20:39:10 +0200

    Fixed background opacity

--- gorilas_upc/src/app/page2.tsx
+++ gorilas_upc/src/app/page2.tsx
@@ -2,11 +2,11 @@ import "../../public/style/style.css";
 
 export default function Home2() {
     return (
-        <div>
-            <div className="bg-heroImage2 h-screen w-screen bg-cover bg-center blur-sm">
+        <div className="bg-heroImage2 h-screen w-screen bg-cover bg-center">
+            <div className="h-screen w-screen bg-black opacity-[0.3]">
             </div>
-            <div className="absolute overflow-hidden z-10 top-[50%] w-screen h-[16rem] gap-y-0">
-                <span className="aa text-[14rem] text-[#ffffff] font-bebas-neue">
+            <div className="absolute overflow-hidden z-10 top-[50%] w-screen h-16vw gap-y-0">
+                <span className="aa text-[#ffffff] font-bebas-neue">
                     <span className="title" id="l1">G</span>
                     <span className="title" id="l2">O</span>
                     <span className="title" id="l3">R</span>
@@ -15,21 +15,21 @@ export default function Home2() {
                     <span className="title" id="l6">A</span>
                     <span className="title" id="l7">S</span>
                 </span>
-                <span className="text-[14rem] text-[#166BCB] font-bebas-neue">
+                <span className="text-[#166BCB] font-bebas-neue">
                     <span className="title" id="l8">U</span>
                     <span className="title" id="l9">P</span>
                     <span className="title" id="l10">C</span>
                 </span>
             </div>
-            <div className="absolute overflow-hidden z-10 top-[50%] w-screen h-[16rem] gap-y-0">
-                <span className="aa2 text-[14rem] text-[#ee7c31] font-bebas-neue">
+            <div className="absolute overflow-hidden z-10 top-[50%] w-screen h-16vw gap-y-0">
+                <span className="aa2 text-[#ee7c31] font-bebas-neue">
                     <span className="title2" id="l11">R</span>
                     <span className="title2" id="l12">U</span>
                     <span className="title2" id="l13">G</span>
                     <span className="title2" id="l14">B</span>
                     <span className="title2" id="l15">Y</span>
                 </span>
-                <span className="text-[14rem] text-[#ffffff] font-bebas-neue">
+                <span className="text-[#ffffff] font-bebas-neue">
                     <span className="title2" id="l16">U</span>
                     <span className="title2" id="l17">P</span>
                     <span className="title2" id="l18">C</span>
