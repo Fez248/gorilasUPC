57ac4fc0fc9c8aeedbab8deda212707349752bb1
Fez248 <ferran.beneitez@estudiantat.upc.edu>
2024-05-17 18:36:11 +0200

    Working on the homePage animation

--- gorilas_upc/src/app/page2.tsx
+++ gorilas_upc/src/app/page2.tsx
@@ -5,23 +5,35 @@ export default function Home2() {
         <div>
             <div className="bg-heroImage2 h-screen w-screen bg-cover bg-center blur-sm">
             </div>
-            <div className="flex flex-col absolute z-10 top-0 w-screen h-screen gap-y-0">
-                <div className="">
-                    <span className="text-[14rem] text-[#ffffff] font-bebas-neue">
-                        <span className="title" id="l1">G</span>
-                        <span className="title" id="l2">O</span>
-                        <span className="title" id="l3">R</span>
-                        <span className="title" id="l4">I</span>
-                        <span className="title" id="l5">L</span>
-                        <span className="title" id="l6">A</span>
-                        <span className="title" id="l7">S</span>
-                    </span>
-                    <span className="text-[14rem] text-[#166BCB] font-bebas-neue">
-                        <span className="title" id="l8">U</span>
-                        <span className="title" id="l9">P</span>
-                        <span className="title" id="l10">C</span>
-                    </span>
-                </div>
+            <div className="absolute overflow-hidden z-10 top-[50%] w-screen h-[16rem] gap-y-0">
+                <span className="aa text-[14rem] text-[#ffffff] font-bebas-neue">
+                    <span className="title" id="l1">G</span>
+                    <span className="title" id="l2">O</span>
+                    <span className="title" id="l3">R</span>
+                    <span className="title" id="l4">I</span>
+                    <span className="title" id="l5">L</span>
+                    <span className="title" id="l6">A</span>
+                    <span className="title" id="l7">S</span>
+                </span>
+                <span className="text-[14rem] text-[#166BCB] font-bebas-neue">
+                    <span className="title" id="l8">U</span>
+                    <span className="title" id="l9">P</span>
+                    <span className="title" id="l10">C</span>
+                </span>
+            </div>
+            <div className="absolute overflow-hidden z-10 top-[50%] w-screen h-[16rem] gap-y-0">
+                <span className="aa2 text-[14rem] text-[#ee7c31] font-bebas-neue">
+                    <span className="title2" id="l1">R</span>
+                    <span className="title2" id="l2">U</span>
+                    <span className="title2" id="l3">G</span>
+                    <span className="title2" id="l4">B</span>
+                    <span className="title2" id="l5">Y</span>
+                </span>
+                <span className="text-[14rem] text-[#ffffff] font-bebas-neue">
+                    <span className="title2" id="l8">U</span>
+                    <span className="title2" id="l9">P</span>
+                    <span className="title2" id="l10">C</span>
+                </span>
             </div>
         </div>
     );
